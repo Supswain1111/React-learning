e89be6e25b262449932822d0d1e9b68176c9697a
Supswain1111 <123567893+Supswain1111@users.noreply.github.com>
2023-05-03 21:25:41 +0530

    component

---
+++ ExpenseItems.js
@@ -0,0 +1,12 @@
+function ExpenseItems( ){
+    return (
+        <div>
+            <h2>Expense Items</h2>
+            <h5>Food Rs 10</h5>
+            <h5>Petrol Rs 100</h5>
+            <h5>Movie Rs 200</h5>
+        </div>
+    );
+}
+
+export default ExpenseItems;
