36caebec2dc8f2b294622099b3e2eb0d05556d2f
Supswain1111 <123567893+Supswain1111@users.noreply.github.com>
2023-05-04 13:54:11 +0530

    second commit adding some component

--- ExpenseItems.js
+++ ExpenseItems.js
@@ -1,10 +1,14 @@
-function ExpenseItems( ){
+import './ExpenseItems.css';
+function ExpenseItems(props ){
+   
     return (
-        <div>
-            <h2>Expense Items</h2>
-            <h5>Food Rs 10</h5>
-            <h5>Petrol Rs 100</h5>
-            <h5>Movie Rs 200</h5>
+        <div className = "expense-item">
+           <div>{props.date.toISOString()}</div>
+           <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+            <h3>{props.location}</h3>
+            <div className='expense-item__price'>${props.amount}</div>
+           </div>
         </div>
     );
 }
